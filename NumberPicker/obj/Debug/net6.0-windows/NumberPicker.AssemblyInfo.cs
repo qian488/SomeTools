018b89970605d7059e8f58b53635fdfe0fa7dff2
千幻笙 <3517576561@qq.com>
2024-08-18 09:21:16 +0800

    补充README

--- NumberPicker/obj/Debug/net6.0-windows/NumberPicker.AssemblyInfo.cs
+++ NumberPicker/obj/Debug/net6.0-windows/NumberPicker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberPicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b588c7eb7d56a656d9750902ad9532c490a030c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberPicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberPicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SomeTools
 自己随便编写的一些小工具玩玩
+### NumberPicker
+简单抽号器
+
+![78d394e1e03545f0ffe204f0564d796](https://raw.githubusercontent.com/qian488/qian_personal_bolg_images/main/img/78d394e1e03545f0ffe204f0564d796.png)
